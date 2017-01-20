6e4376fcdb99071e9fef03c7d09a511859f90289
CasaboneLucas <casabonelucas@hotmail.com>
2017-01-20 20:53:06 -0300

    avances en estructura

--- Web/app/login/login.js
+++ Web/app/login/login.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('Login', ['Core'])
+angular.module('Login', ['Core', 'Layout'])
     .config(function($stateProvider) {
         $stateProvider
             .state('login', {
--- Web/app/researchers/researchers.js
+++ Web/app/researchers/researchers.js
@@ -1,3 +1,21 @@
-/**
- * Created by agustin on 20/01/2017.
- */
+'use strict';
+
+angular.module('Researchers', ['Core'])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('researchers', {
+                url: "/researchers",
+                abstract: true,
+                template: "<ui-view />"
+            })
+            .state('researchers.list', {
+                url: "/",
+                templateUrl: "researchers/index.html",
+                controller: 'researchers.list'
+            })
+            .state('researchers.new', {
+                url: "researchers/new",
+                templateUrl: "researchers/edit.html",
+                controller: 'researchers.new'
+            });
+    });
