cedf2feb13c8361979c8893583cbf72efc4ab0ba
CasaboneLucas <casabonelucas@hotmail.com>
2017-02-19 16:07:18 -0300

    Datepicker funcionando.

--- Web/app/core/core.js
+++ Web/app/core/core.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('Core', ['ngMaterial','ui.router', 'ngCookies', 'ngMessages']);
+angular.module('Core', ['ngMaterial','ui.router', 'ngCookies', 'ngMessages', '720kb.datepicker']);
