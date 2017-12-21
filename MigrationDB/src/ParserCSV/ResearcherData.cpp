2505b3f2b483011d62ee228a2acc1cc185362e1d
alejandroalvarez <tec.alejandroalvarez@gmail.com>
2017-12-21 16:16:10 -0300

    Se agrego la clase ResearcherData

---
+++ MigrationDB/src/ParserCSV/ResearcherData.cpp
@@ -0,0 +1,27 @@
+#include "ResearcherData.h"
+#include "FormationData.h"
+#include "PersonalData.h"
+#include "PositionData.h"
+
+ResearcherData::ResearcherData() {
+}
+FormationData ResearcherData::getFormationData() const{
+	return this->formationData;
+}
+void ResearcherData::setFormationData(FormationData formationData){
+	this->formationData = formationData;
+}
+PersonalData ResearcherData::getPersonalData() const{
+	return this->personalData;
+}
+void ResearcherData::setPersonalData(PersonalData personalData){
+	this->personalData = personalData;
+}
+PositionData ResearcherData::getPositionData() const{
+	return this->positionData;
+}
+void ResearcherData::setPositionData(PositionData positionData){
+	this->positionData = positionData;
+}
+ResearcherData::~ResearcherData() {
+}
